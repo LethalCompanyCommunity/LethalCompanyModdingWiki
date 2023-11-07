d023adc7f2cf356b7d546c744d326cbb896000ab
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-07 14:20:54 +0100

    Add template project

---
+++ LethalCompanyPluginTemplate/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace LethalCompanyPluginTemplate
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
